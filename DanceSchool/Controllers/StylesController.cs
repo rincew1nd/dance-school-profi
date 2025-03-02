802161f3e900fd086ba2804e8a52ccbfae145221
Viacheslav Titov <rincew1nd@ya.ru>
2025-03-03 00:17:53 +0300

    Initial commit

---
+++ DanceSchool/Controllers/StylesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DanceSchool.Models;
+
+namespace DanceSchool.Controllers
+{
+    public class StylesController : Controller
+    {
+        private DanceSchoolEntities db = new DanceSchoolEntities();
+
+        // GET: Styles
+        public ActionResult Index()
+        {
+            return View(db.Styles.ToList());
+        }
+
+        // GET: Styles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Style style = db.Styles.Find(id);
+            if (style == null)
+            {
+                return HttpNotFound();
+            }
+            return View(style);
+        }
+
+        // GET: Styles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Styles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Style style)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Styles.Add(style);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(style);
+        }
+
+        // GET: Styles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Style style = db.Styles.Find(id);
+            if (style == null)
+            {
+                return HttpNotFound();
+            }
+            return View(style);
+        }
+
+        // POST: Styles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Style style)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(style).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(style);
+        }
+
+        // GET: Styles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Style style = db.Styles.Find(id);
+            if (style == null)
+            {
+                return HttpNotFound();
+            }
+            return View(style);
+        }
+
+        // POST: Styles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Style style = db.Styles.Find(id);
+            db.Styles.Remove(style);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DanceSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DanceSchool.Startup))]
+namespace DanceSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
